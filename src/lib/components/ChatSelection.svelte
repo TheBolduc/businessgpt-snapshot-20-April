baf7c8285ab0dd627a3839731c686269ea7ab7d7
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-21 15:35:16 +0200

    Create ChatSelection.svelte

---
+++ src/lib/components/ChatSelection.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let chatbotOptions = [];
+  export let onSelect;
+</script>
+
+<div class="flex flex-wrap gap-4 justify-center">
+  {#each chatbotOptions as chatbot}
+    <button
+      class="btn btn-primary"
+      on:click={() => onSelect(chatbot)}
+    >
+      {chatbot.name}
+    </button>
+  {/each}
+</div>
