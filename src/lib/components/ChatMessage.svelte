09d74e8b0865660cb0e5369357efdd13df6bbeb3
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-21 19:37:02 +0200

    Update ChatMessage.svelte

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -2,13 +2,14 @@
   import type { ChatCompletionRequestMessageRoleEnum } from 'openai'
   export let type: ChatCompletionRequestMessageRoleEnum
   export let message: string
+  export let chatbotName = 'Tommy'; // Add this line to define the chatbotName prop with a default value
   $: formattedMessage = message.replace(/(\d+[\.-])/g, '<br><br>$1')
 </script>
 
 <div class="flex {type === 'user' ? 'justify-end' : 'justify-start'} py-2">
   <div class="hidden md:block w-10 min-w-10 h-10 flex-none">
     {#if type === 'assistant'}
-      <img src="https://ui-avatars.com/api/?name=Tommy" alt="Tommy avatar" class="rounded-full w-10 h-10 object-cover" />
+      <img src="https://ui-avatars.com/api/?name={chatbotName}" alt="{chatbotName} avatar" class="rounded-full w-10 h-10 object-cover" /> <!-- Change the name and alt attributes to use the chatbotName variable -->
     {/if}
     {#if type === 'user'}
       <img src="https://ui-avatars.com/api/?name=User" alt="User avatar" class="rounded-full w-10 h-10 object-cover" />
@@ -16,7 +17,7 @@
   </div>
   <div class="px-2">
     {#if type === 'assistant'}
-      <div class="text-sm text-gray-500">Tommy</div>
+      <div class="text-sm text-gray-500">{chatbotName}</div> <!-- Change the hardcoded "Tommy" to the chatbotName variable -->
     {/if}
     {#if type === 'user'}
       <div class="text-sm text-gray-500">User</div>
