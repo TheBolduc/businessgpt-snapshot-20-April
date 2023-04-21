c8d9d3fff62d545508bd93550bccdde3bb2293f1
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-21 20:10:21 +0200

    Create Sidebar.svelte

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { goto } from '$app/navigation';
+
+  function navigateToHome() {
+    goto('/');
+  }
+</script>
+
+<aside class="fixed left-0 top-0 h-full w-64 bg-gray-100 p-4">
+  <button
+    class="btn btn-primary"
+    on:click={navigateToHome}
+  >
+    Chatbots
+  </button>
+</aside>
