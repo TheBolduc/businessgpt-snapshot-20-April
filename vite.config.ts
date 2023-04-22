452c08335669c3132746707dd22fcef878cedb2c
TheBolduc <97337225+TheBolduc@users.noreply.github.com>
2023-04-22 14:58:46 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,11 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-  plugins: [
-    svelte({
-      compilerOptions: {
-        dev: !production,
-      },
-      emitCss: true,
-    }),
-    sveltekit(),
-  ],
-  optimizeDeps: {
-    exclude: ['businessgpt-snapshot-20-april/src/lib/components/Sidebar.svelte']
-  },
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 };
 
 export default config;
